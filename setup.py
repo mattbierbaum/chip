3ba7ea843f00fc024803ce3d641cf1e9b326367b
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-08-25 23:33:44 -0400

    making a package out of it

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from __init__ import __version__
 
 setup(
     name='chip',
-    version=__version__,
+    version="0.1.0",
     description='OpenKIM package distribution utilities.',
     author='Matt Bierbaum',
     author_email='mkb72@cornell.edu',
     url='http://www.python.org/sigs/distutils-sig/',
     packages=['chip'],
-    scripts=['chip']
+    scripts=['bin/chip']
 )
